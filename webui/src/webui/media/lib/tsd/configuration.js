bd222f1343347f0ddf804050025ddab9674b0e44
Kai Ren <kair@cs.cmu.edu>
2011-06-22 14:38:51 -0400

    update django framework

--- webui/src/webui/media/lib/tsd/configuration.js
+++ webui/src/webui/media/lib/tsd/configuration.js
@@ -6,9 +6,9 @@ var NumReducer=4;
 var NumServers=64;
 var MetricList = ["cpu_user", "cpu_system", "vmrss", "vmsize", "readbytesrate", "writebytesrate"];
 var ProcessList = ["DataNode", "TaskTracker"];
-var MetricYLabel = {cpu_user:"USER CPU", cpu_system:"SYSTEM CPU", vmrss:"Resident Memory (Bytes)",
-					vmsize:"Virtual Memory (Bytes)", readbytesrate:"Disk I/O Throughput (Bytes)",
-					writebytesrate:"Disk I/O Throughput (Bytes)"};
+var MetricYLabel = {cpu_user:"User CPU", cpu_system:"System CPU", vmrss:"Resident Memory (Bytes)",
+					vmsize:"Virtual Memory (Bytes)", readbytesrate:"Disk I/O Read Throughput (Bytes)",
+					writebytesrate:"Disk I/O Write Throughput (Bytes)"};
 var ServerList=new Array();
 for (var i = 1; i <= NumServers; ++i) {
 	ServerList.push("cloud"+i);
